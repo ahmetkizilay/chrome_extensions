2afdf4f53e468102e6bf42cad8da2675ad763c93
Ahmet Kizilay <yourstruly@ahmetkizilay.com>
2011-05-14 14:50:39 +0300

    corrected and optimized storage reading methods

--- readitlater/js/ril.js
+++ readitlater/js/ril.js
@@ -1,17 +1,23 @@
 function setUnreadCount() {
     var stored = localStorage["ReadItLater"];
-    if(!stored) stored = "";
-    var storedLength = stored.split("\n").length;
-    if(storedLength) {
-	storedLength = (storedLength - 1) / 2;
+    var storedLength = 0;
+    if(!stored == "") {
+	storedLength = stored.split("\n").length;
+	storedLength = (storedLength) / 2;
     }
     chrome.browserAction.setBadgeText({text: String(storedLength)});
 }
 
 function saveLink() {
     var stored = localStorage["ReadItLater"];
-    if(!stored) stored = "";
-    stored += $("#newTitle").val() + "\n" + $("#newLink").val() + "\n";
+    if(stored == "") {
+	stored = "";
+    }
+    else {
+	stored += "\n";
+    }
+    
+    stored += $("#newTitle").val() + "\n" + $("#newLink").val();
     localStorage["ReadItLater"] = stored;
 
     setUnreadCount();
--- readitlater/js/ril_pop.js
+++ readitlater/js/ril_pop.js
@@ -1,6 +1,24 @@
+function createItemArea(title, link) {
+    $('#storedItems').append("<div class='item'><a href='" + link + "'>" + title + "</a><br/></div>");
+}
+
+function createItems() {
+    
+    var storedItems = localStorage["ReadItLater"];
+    if(storedItems != "") {
+	var storedList = storedItems.split('\n');
+	for(var i = 0; i < storedList.length; i = i + 2) {
+	    createItemArea(storedList[i], storedList[i + 1]);
+	}
+    }
+}
+
 $(document).ready(function(){
     chrome.tabs.getSelected(null, function(tab) {
 	$("#newTitle").val(tab.title);
 	$("#newLink").val(tab.url);
     });
+
+    createItems();
 });
+
